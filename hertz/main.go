b57bfd58b10e01df84bd29c0d098a68ad334a43c
luoqing <tsingroo@gmail.com>
2025-11-06 20:00:58 +0800

    添加hertz代码

---
+++ hertz/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+)
+
+func main() {
+	hServer := server.Default()
+
+	hServer.GET("/ping", func(ctx context.Context, c *app.RequestContext) {
+		c.String(consts.StatusOK, "pong")
+	})
+	hServer.GET("/hello/:name", func(ctx context.Context, c *app.RequestContext) {
+		c.String(consts.StatusOK, "Hello "+c.Param("name"))
+	})
+
+	hServer.Spin()
+}
