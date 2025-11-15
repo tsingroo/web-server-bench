d2b714024be14f211f41a507edc15e090afdb238
luoqing <tsingroo@gmail.com>
2025-11-15 14:54:10 +0800

    添加flask测试结果

--- kratos/internal/service/greeter.go
+++ kratos/internal/service/greeter.go
@@ -5,7 +5,6 @@ import (
 
 	v1 "kratos_bench/api/helloworld/v1"
 	"kratos_bench/internal/biz"
-	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 // GreeterService is a greeter service.
@@ -26,10 +25,15 @@ func (s *GreeterService) SayHello(ctx context.Context, in *v1.HelloRequest) (*v1
 	if err != nil {
 		return nil, err
 	}
-	return &v1.HelloReply{Message: "my name is " + g.Hello}, nil
+	return &v1.HelloReply{Message: "Hello " + g.Hello}, nil
 }
 
 // Ping implements helloworld.GreeterServer.
-func (s *GreeterService) Ping(ctx context.Context, in *emptypb.Empty) (*v1.PongReply, error) {
-	return &v1.PongReply{Message: "pong"}, nil
+func (s *GreeterService) Ping(ctx context.Context, in *v1.PingRequest) (*v1.PingReply, error) {
+	return &v1.PingReply{Message: "pong"}, nil
+}
+
+// SayHelloName implements helloworld.GreeterServer.
+func (s *GreeterService) SayHelloName(ctx context.Context, in *v1.HelloNameRequest) (*v1.HelloNameReply, error) {
+	return &v1.HelloNameReply{Message: "my name is " + in.Name}, nil
 }
